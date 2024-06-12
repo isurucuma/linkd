cd22965043b84ce42448be6bb8f8e496060232a0
isuru <isurucuma@gmail.com>
2024-06-13 00:55:21 +0530

    linkd server initial routing done

---
+++ cmd/linkd/linkd.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"errors"
+	link "linkd"
+	"log/slog"
+	"net/http"
+	"time"
+)
+
+func main() {
+	const timeout = 10 * time.Second
+	const addr = "localhost:8080"
+
+	log := slog.With("app", "linkd")
+	slog.SetDefault(log)
+
+	log.Info("starting", "addr", addr)
+
+	links := link.NewServer()
+	handler := http.TimeoutHandler(links, time.Second, "timeout")
+
+	srv := &http.Server{
+		Addr:        addr,
+		ReadTimeout: timeout * 2,
+		IdleTimeout: timeout * 4,
+		Handler:     handler,
+	}
+
+	err := srv.ListenAndServe()
+	if errors.Is(err, http.ErrServerClosed) { // ErrServerClosed is a expedted if not that is an unexpected error
+		log.Error("server closed unexpectedly", "message", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module linkd
+
+go 1.22.1
